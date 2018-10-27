1fb62d92f790efa4e238dd8aac6005fa6d375b37
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-27 11:02:59 -0600

    Continue refactor

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,5 +1,6 @@
 class MostDangerousDayController < ApplicationController
   def index
-    @search = DangerousDaySearch.new(params["start_date"], params["end_date"])
+    # @search = DangerousDaySearch.new(params["start_date"], params["end_date"])
+    @search = DangerousDayFacade.new(params["start_date"], params["end_date"])
   end
 end
