6b7ce4e2312f6829e3292ec605c52548d7d63050
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-27 11:38:03 -0600

    Complete refactor of date formatting

--- app/facades/dangerous_day_facade.rb
+++ app/facades/dangerous_day_facade.rb
@@ -1,12 +1,10 @@
 class DangerousDayFacade
-  attr_reader :formatted_start_date, :formatted_end_date
+  attr_reader :start_date, :end_date
 
   def initialize(start_date, end_date)
     @start_date = start_date
-    @formatted_start_date = start_date.to_datetime.strftime("%B %-d, %Y")
     @end_date = end_date
-    @formatted_end_date = end_date.to_datetime.strftime("%B %-d, %Y")
-    @most_dangerous_day = most_dangerous_day.to_s.to_datetime.strftime("%B %-d, %Y")
+    @most_dangerous_day = most_dangerous_day.to_s
   end
 
   def most_dangerous_day
@@ -15,5 +13,9 @@ class DangerousDayFacade
 
   def neos
     @neos ||= DangerousDaySearch.new(@start_date, @end_date).neos
+  end
+
+  def formatted(date)
+    date.to_datetime.strftime("%B %-d, %Y")
   end
 end
