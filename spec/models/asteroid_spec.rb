47fdd557aeb8f0600ae10f21c16a4f59eb667a72
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-27 18:07:53 -0600

    Add more test for user story 2, including model tests for asteroid

--- spec/models/asteroid_spec.rb
+++ spec/models/asteroid_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Asteroid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to(:favorite)}
+  end
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+  end
 end
--- spec/requests/api/v1/user/favorites_spec.rb
+++ spec/requests/api/v1/user/favorites_spec.rb
@@ -8,7 +8,36 @@ describe 'Favorites API' do
     get "/api/v1/user/favorites?api_key=#{user.api_key}"
 
     favorites = JSON.parse(response.body)
+
     expect(response).to be_successful
     expect(favorites.count).to eq(3)
+
+    expect(favorites[0]).to have_key("id")
+    expect(favorites[0]).to have_key("neo_reference_id")
+    expect(favorites[0]).to have_key("user_id")
+    expect(favorites[0]).to have_key("asteroid")
+    expect(favorites[0]["asteroid"]).to have_key("name")
+    expect(favorites[0]["asteroid"]).to have_key("is_potentially_hazardous_asteroid")
+  end
+
+  it 'sends the info expected by the user story' do
+    user = create(:user, id: 1)
+    favorite = create(:favorite, id: 1, user_id: user.id, neo_reference_id: "2153306")
+    create(:asteroid, name: "153306 (2001 JL1)", is_potentially_hazardous_asteroid: false, favorite_id: favorite.id)
+
+    get "/api/v1/user/favorites?api_key=#{user.api_key}"
+
+    favorites = JSON.parse(response.body, symbolize_names: true)
+
+    expected_info = [{  "id":1,
+                        "neo_reference_id": "2153306",
+                        "user_id": 1,
+                        "asteroid": { "name": "153306 (2001 JL1)",
+                                      "is_potentially_hazardous_asteroid": false,
+                                    }
+                    }]
+    expect(response).to be_successful
+    expect(favorites.count).to eq(1)
+    expect(favorites).to eq(expected_info)
   end
 end
